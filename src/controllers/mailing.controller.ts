ef62bc34009c31739fb5ea0ed31273b713325081
cmandujano <carlosmandujano.v@gmail.com>
2026-04-25 12:24:55 -0600

    update confirm email template

--- src/controllers/mailing.controller.ts
+++ src/controllers/mailing.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { sendEmail } from '../services/email.service';
-import { welcomeTemplate } from '../utils/email-templates/welcome.template';
+//import { welcomeTemplate } from '../utils/email-templates/welcome.template'; for now we dont need this template
+import { accountConfirmationTemplate } from '../utils/email-templates/confirm-account.template';
 
 
 export const sendWelcomeEmail = async (req: Request, res: Response) => {
@@ -9,11 +10,11 @@ export const sendWelcomeEmail = async (req: Request, res: Response) => {
     try {
         await sendEmail({
             to: email,
-            subject: 'Bienvenido a Odontofy',
-            html: welcomeTemplate(name),
+            subject: 'Confirma tu cuenta en Odontofy',
+            html: accountConfirmationTemplate(name, ''),
         });
 
-        res.status(200).json({ ok: true, msg: 'Correo de bienvenida enviado' });
+        res.status(200).json({ ok: true, msg: 'Correo de confirmación enviado' });
     } catch (error) {
         console.error('Error al enviar el correo:', error);
         res.status(500).json({ ok: false, msg: 'Error al enviar el correo' });
